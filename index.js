407b4abf58362a05aa1233ef9b35e34e5c8999b5
Sima-Akther <saimaislambdk@gmail.com>
2025-08-28 07:12:42 +0600

    add js code

--- index.js
+++ index.js
@@ -8,13 +8,64 @@
 
 const hearts = document.getElementsByClassName(' heart-btn');
 
-let count = 0;
+let counts = 0;
 for (const heart of hearts) {
     heart.addEventListener('click', ()=>{
-        count++;
+        counts++;
       const display = document.getElementById('add');
-      display.textContent=count;
+      display.textContent=counts;
     })
 }
 
 
+
+
+const cardbtns = document.getElementsByClassName('card-btn');
+
+let count=100;
+ for (const cardbtn of cardbtns) {
+    cardbtn.addEventListener('click', ()=>{
+
+
+  count-=20;
+    const decreat = document.getElementById('decreat');
+    decreat.textContent=count;
+
+    let date = new Date();
+
+  if(count>0){
+    alert(`${count-=0}`);
+  }
+  if(count<0){
+    alert(`stop ${count=0}`)
+  }
+
+       const locetion = cardbtn.parentNode.parentNode.children[1].children[0].innerText;
+        const phoneNumber = cardbtn.parentNode.parentNode.children[2].children[0].innerText;
+        console.log(phoneNumber);
+        
+        const cardContainer = document.getElementById('call-history-card')
+        
+        const newCard = document.createElement('div');
+        newCard.innerHTML=`
+         <div class="flex justify-between  bg-slate-100 p-3 rounded-md">
+            <div>
+              <h1 class="font-bold">${locetion}</h1>
+              <p>${phoneNumber}</p>
+            </div>
+            <div>
+                <h1>${date.toLocaleTimeString()}</h1>
+            </div>
+          </div>
+        `;
+        cardContainer.append(newCard);
+    })
+ }
+
+document.getElementById('btn-clear').addEventListener('click', ()=>{
+     const cardContainer = document.getElementById('call-history-card');
+     cardContainer.innerHTML = '';
+})
+
+
+ 
