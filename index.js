ed27b7373abbf57ffe982b49d7b52255b84fb13e
Sima-Akther <saimaislambdk@gmail.com>
2025-08-29 23:38:00 +0600

    update index.js file

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
 
-// let count = 0;
-// document.getElementById('heart').addEventListener('click', ()=>{
-//   count++;
-//   const display = document.getElementById('add');
-//   display.textContent=count
-// })
 
 const hearts = document.getElementsByClassName(' heart-btn');
 
